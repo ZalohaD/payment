0c41bf9fbc24c66ee67004ef54cf6f4ebe61a65b
ZalohaD <74576798+ZalohaD@users.noreply.github.com>
2023-02-18 14:54:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  
+  // Get the form data
+  const username = form.username.value;
+  const email = form.email.value;
+  const phone = form.phone.value;
+  const amount = form.amount.value;
+  const currency = form.currency.value;
+
+  // Create the Volt API request
+  const url = 'https://api.volt.io/payment';
+  const data = {
+    api_username: 'ave637b436fadff01.86527928@volt.io',
+    api_password: '%Ntg03f@$X4FC*96M&!l2JLq8Rj#1dh1',
+    client_id: '00df623f-fb0c-4c5a-b2a7-88d469d08809',
+    client_secret: '57999661-a43b-4fde-bf76-3d11e61f4b16',
+    username,
+    email,
+    phone,
+    amount,
+    currency,
+  };
+
+  // Make the Volt API request using fetch
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Redirect to the link with the POST request
+    const link = result.link;
+    const redirectForm = document.createElement('form');
+    redirectForm.method = 'post';
+    redirectForm.action = link;
+    document.body.appendChild(redirectForm);
+    redirectForm.submit();
+  })
+  .catch(error => console.error(error));
+});
